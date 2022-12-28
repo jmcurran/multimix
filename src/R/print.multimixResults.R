8dec5e85ba7ccc40197124825e9253504cd70ac6
jmcurran <james.m.curran@gmail.com>
2022-12-28 17:31:51 +1300

    Altered print method to display more lines of the log-likelihood

--- src/R/print.multimixResults.R
+++ src/R/print.multimixResults.R
@@ -2,19 +2,42 @@
 #'
 #' @param x an object of class \code{multimixResults}---see \code{\link{mmain}}
 #'   for a description.
+#' @param dispLL display the last few iterations of the log-likelihood. If 
+#'   \code{TRUE} then the last 5 iterations will be displayed by default.
+#'   Alternatively, a positive integer can be supplied. If this exceeds the 
+#'   number of actual iterations, the output will be truncated.
 #' @param ... other parameters passed to \code{print}. Not currently used.
 #'
 #' @return 
 #' @export
 #'
 #' @examples
-print.multimixResults = function(x, ...){
+print.multimixResults = function(x, dispLL = FALSE, ...){
   if(!is(x, "multimixResults")){
     stop("x must be of class multimixResults")
   }
   
+  if(!is.logical(dispLL) & !is.numeric(dispLL)){
+    stop("dispLL must be either: TRUE, FALSE or an integer greater than zero.")
+  }
+  
+  if(is.logical(dispLL)){
+    if(!dispLL){
+      numLLlines = 0
+    }else{
+      numLLlines = 5
+    }
+  }else{
+    if(dispLL <= 0){
+      stop("dispLL must be a positive integer")
+    }else{
+      numLLlines = floor(dispLL)
+    }
+  }
+  
   finalResult = x$results[nrow(x$results),]
-  cat(paste0("The algorithm terminated after ", finalResult["Iteration"], 
+  nIterations = finalResult["Iteration"]
+  cat(paste0("The algorithm terminated after ", nIterations, 
              " iterations\n"))
   cat(paste0("The final value of the log-likelihood is ", 
             sprintf("%.2f", finalResult["Log-likelihood"]), "\n\n"))
@@ -27,5 +50,15 @@ print.multimixResults = function(x, ...){
   for(i in 1:numClusters){
     cat(sprintf("%-4d  %6.4f\n", i, finalResult[i + 1]))
   }
-  cat("\n\n")
+  cat("\n")
+  
+  if(numLLlines > 0){
+    numLines = min(floor(numLLlines), nIterations)
+    llLines = tail(x$results[, c("Iteration", "Log-likelihood")], numLLlines)
+    cat(paste0("Last ", numLines, " values of the log-likelihood:\n\n"))
+    cat("Iter.  Log-lik.\n")
+    cat("-----  --------\n")
+    cat(paste0(sprintf("%5d %.2f", llLines[,1], llLines[,2]),
+               collapse = "\n"))
+  }
 }
