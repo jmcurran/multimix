6b07604c70707e9c376e07d64a9f95294b9c44ef
jmcurran <james.m.curran@gmail.com>
2022-12-29 14:55:07 +1300

    fixed the printing in print.multimixResults to print probabilities and not likelihoods

--- src/R/print.multimixResults.R
+++ src/R/print.multimixResults.R
@@ -2,7 +2,7 @@
 #'
 #' @param x an object of class \code{multimixResults}---see \code{\link{mmain}}
 #'   for a description.
-#' @param dispLL display the last few iterations of the log-likelihood. If 
+#' @param n display the last few iterations of the group probabilities. If 
 #'   \code{TRUE} then the last 5 iterations will be displayed by default.
 #'   Alternatively, a positive integer can be supplied. If this exceeds the 
 #'   number of actual iterations, the output will be truncated.
@@ -12,26 +12,26 @@
 #' @export
 #'
 #' @examples
-print.multimixResults = function(x, dispLL = FALSE, ...){
+print.multimixResults = function(x, n = FALSE, ...){
   if(!is(x, "multimixResults")){
     stop("x must be of class multimixResults")
   }
   
-  if(!is.logical(dispLL) & !is.numeric(dispLL)){
-    stop("dispLL must be either: TRUE, FALSE or an integer greater than zero.")
+  if(!is.logical(n) & !is.numeric(n)){
+    stop("n must be either: TRUE, FALSE or an integer greater than zero.")
   }
   
-  if(is.logical(dispLL)){
-    if(!dispLL){
-      numLLlines = 0
+  if(is.logical(n)){
+    if(!n){
+      numLines = 0
     }else{
-      numLLlines = 5
+      numLines = 5
     }
   }else{
-    if(dispLL <= 0){
-      stop("dispLL must be a positive integer")
+    if(n <= 0){
+      stop("n must be a positive integer")
     }else{
-      numLLlines = floor(dispLL)
+      numLines = floor(n)
     }
   }
   
@@ -52,13 +52,27 @@ print.multimixResults = function(x, dispLL = FALSE, ...){
   }
   cat("\n")
   
-  if(numLLlines > 0){
-    numLines = min(floor(numLLlines), nIterations)
-    llLines = tail(x$results[, c("Iteration", "Log-likelihood")], numLLlines)
-    cat(paste0("Last ", numLines, " values of the log-likelihood:\n\n"))
-    cat("Iter.  Log-lik.\n")
-    cat("-----  --------\n")
-    cat(paste0(sprintf("%5d %.2f", llLines[,1], llLines[,2]),
-               collapse = "\n"))
+  printProbs = function(probs){
+    fmtString = paste0("%4d  ", 
+                       paste0(rep("   %6.4f", numClusters), collapse = " "),
+                       "\n")
+    inputsString = paste0("probs[, ", 1:(numClusters + 1), "]", collapse = ", ")
+    cmd = paste0("sprintf(\"", fmtString, "\", ", inputsString, ")")
+    eval(parse(text = cmd))
+  }
+  
+  if(numLines > 0){
+    numLines = min(floor(numLines), nIterations)
+    probRows = tail(x$results[, c(numClusters + 2, 2:(numClusters + 1))], numLines)
+    cat(paste0("Last ", numLines, " values of the group probabilities:\n\n"))
+    if(numClusters >= 10){
+      cat(paste0("Iter.  ", paste0("Pr(G = ", sprintf("%2d", 1:numClusters), ")", collapse = " "), "\n"))
+      cat(paste0("-----  ", paste0(rep("----------", numClusters), collapse = " "), "\n "))
+    }else{
+      cat(paste0("Iter.  ", paste0("Pr(G = ", 1:numClusters, ")", collapse = " "), "\n"))
+      cat(paste0("-----  ", paste0(rep("---------", numClusters), collapse = " "), "\n "))
+    }
+    
+    cat(printProbs(probRows))
   }
 }
