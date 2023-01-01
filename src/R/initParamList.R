e251363a7c1da382439693d5ad975313753e4ac8
jmcurran <james.m.curran@gmail.com>
2023-01-01 13:01:54 +1300

    Changed mind about how initParamsNew works, and have put the declaration of the list an the assignment of local variables at the end of the procedure. I think this makes it a little easier to read

--- src/R/initParamList.R
+++ src/R/initParamList.R
@@ -134,32 +134,30 @@ initParamList <- function(D, Z) {
 
 initParamListNew <- function(D, Z) {
   len.cdep = length(D$cdep)
-  P = list(
-    dstat = vector("list", length(D$dlink)),
-    ldstat = vector("list", length(D$ldlink)),
-    cstat = vector("list", length(D$cdep)),
-    cstat2 = vector("list", len.cdep),
-    cvar = vector("list", len.cdep),
-    cpstat = vector("list", len.cdep)
-  )
+  dstat = vector("list", length(D$dlink))
+  ldstat = vector("list", length(D$ldlink))
+  cstat = vector("list", length(D$cdep))
+  cstat2 = vector("list", len.cdep)
+  cvar = vector("list", len.cdep)
+  cpstat = vector("list", len.cdep)
   ccov = vector("list", len.cdep)
   
   
-  P$lcstat <- 
-    P$lcstat2 <- 
-    P$lcpstat <-
+  lcstat <- 
+    lcstat2 <- 
+    lcpstat <-
     lcvar <-
     lccov <- replicate(length(D$lcdep), list())
  
-  P$MVMV <- list()
+  MVMV <- list()
   for (i in seq_along(D$cdep)) {
-    P$MVMV[[i]] <- list()
+    MVMV[[i]] <- list()
     for (j in seq_len(D$numClusters)) {
-      P$MVMV[[i]][[j]] <- diag(length(D$cdep[[i]]))
+      MVMV[[i]][[j]] <- diag(length(D$cdep[[i]]))
     }
   }
   
-  P$LMV <- list()
+  LMV <- list()
   for (i in seq_along(D$lcdep)) {
     D$LMV[[i]] <- list()
     
@@ -168,42 +166,42 @@ initParamListNew <- function(D, Z) {
     }
   }
   
-  P$pistat <- colSums(Z) / D$n ## check - can this be replaced with colMeans
+  pistat <- colSums(Z) / D$n ## check - can this be replaced with colMeans
   
-  P$W <- Z %*% diag(1 / (D$n * P$pistat))
+  W <- Z %*% diag(1 / (D$n * pistat))
   
   for (i in seq_along(D$dlink)){ 
-    P$dstat[[i]] <- crossprod(P$W, D$dvals[[i]])
+    dstat[[i]] <- crossprod(W, D$dvals[[i]])
   }
   
   for (i in seq_along(D$lcdisc)){
-    P$ldstat[[i]] <- crossprod(P$W, D$ldvals[[i]])
+    ldstat[[i]] <- crossprod(W, D$ldvals[[i]])
   }
   
-  P$ostat <- crossprod(P$W, D$ovals)
-  P$ostat2 <- crossprod(P$W, D$ovals2)
-  P$ovar <- P$ostat2 - P$ostat^2
+  ostat <- crossprod(W, D$ovals)
+  ostat2 <- crossprod(W, D$ovals2)
+  ovar <- ostat2 - ostat^2
   
   for (i in seq_along(D$cdep)) {
-    P$cstat[[i]] <- crossprod(P$W, D$cvals[[i]])
-    P$cstat2[[i]] <- crossprod(P$W, D$cvals2[[i]])
-    P$cpstat[[i]] <- crossprod(P$W, D$cprods[[i]])
+    cstat[[i]] <- crossprod(W, D$cvals[[i]])
+    cstat2[[i]] <- crossprod(W, D$cvals2[[i]])
+    cpstat[[i]] <- crossprod(W, D$cprods[[i]])
   }
   
   for (i in seq_along(D$lcdep)) {
     for (j in seq_len(D$ldlevs[i])) {
       group <-D$ ldvals[[i]][, j] == 1
-      gtot <- colSums(P$W[group, ])
+      gtot <- colSums(W[group, ])
       ### or maybe pmin(colsums(W[group,]), minpstar)
-      WW <- P$W[group, ] %*% diag(1 / gtot)
-      P$lcstat[[i]][[j]] <- crossprod(WW, D$lcvals[[i]][group, ])
-      P$lcstat2[[i]][[j]] <- crossprod(WW, D$lcvals2[[i]][group, ])
-      P$lcpstat[[i]][[j]] <- crossprod(WW, D$lcprods[[i]][group, ])
+      WW <- W[group, ] %*% diag(1 / gtot)
+      lcstat[[i]][[j]] <- crossprod(WW, D$lcvals[[i]][group, ])
+      lcstat2[[i]][[j]] <- crossprod(WW, D$lcvals2[[i]][group, ])
+      lcpstat[[i]][[j]] <- crossprod(WW, D$lcprods[[i]][group, ])
     }
   }
   
-  P$cvar <- P$cstat
-  ccov <- P$cpstat
+  cvar <- cstat
+  ccov <- cpstat
   
   for (i in seq_along(D$cdep)) {
     lcdi <- length(D$cdep[[i]])
@@ -212,25 +210,25 @@ initParamListNew <- function(D, Z) {
     
     for (j in 1:D$numClusters) {
       for (k in seq_along(D$cdep[[i]])) {
-        P$MVMV[[i]][[j]][k, k] = P$cvar[[i]][j, k]
+        MVMV[[i]][[j]][k, k] = cvar[[i]][j, k]
       }
     }
     
     for (ii in seq_len(nxp)) {
-      ccov[[i]][, ii] <- (P$cpstat[[i]][, ii] - P$cstat[[i]][, left(ii)] * P$cstat[[i]][, right(ii)])
+      ccov[[i]][, ii] <- (cpstat[[i]][, ii] - cstat[[i]][, left(ii)] * cstat[[i]][, right(ii)])
     }
     
     for (j in 1:D$numClusters) {
       for (ii in seq_len(nxp)) {
-        P$MVMV[[i]][[j]][left(ii), right(ii)] <-
-          P$MVMV[[i]][[j]][right(ii), left(ii)] <- 
+        MVMV[[i]][[j]][left(ii), right(ii)] <-
+          MVMV[[i]][[j]][right(ii), left(ii)] <- 
           ccov[[i]][j, ii]
       }
     }
   }
   
-  lcvar <- P$lcstat
-  lccov <- P$lcpstat
+  lcvar <- lcstat
+  lccov <- lcpstat
   
   for (i in seq_along(D$lcdep)) {
     lcdi <- length(D$lcdep[[i]]) - 1
@@ -240,32 +238,47 @@ initParamListNew <- function(D, Z) {
       Temp <- rep(0, lcdi)
     
       for (lv in seq_len(D$ldlevs[i])) {
-        lcvar[[i]][[lv]] <- P$lcstat2[[i]][[lv]] - P$lcstat[[i]][[lv]]^2
+        lcvar[[i]][[lv]] <- lcstat2[[i]][[lv]] - lcstat[[i]][[lv]]^2
         
         for (k in seq_len(lcdi)) {
-          Temp[k] <- Temp[k] + lcvar[[i]][[lv]][j, k] * P$ldstat[[i]][j, lv]
+          Temp[k] <- Temp[k] + lcvar[[i]][[lv]][j, k] * ldstat[[i]][j, lv]
         }  #k
       }  #lv  
       
-      diag(P$LMV[[i]][[j]]) <- Temp
+      diag(LMV[[i]][[j]]) <- Temp
       M <- diag(Temp, nrow = length(Temp))
       
       for (lv in seq_len(D$ldlevs[i])) {
         for (ii in seq_len(nxp)) {
-          lccov[[i]][[lv]][, ii] <- (P$lcpstat[[i]][[lv]][, ii] - 
-                                       P$lcstat[[i]][[lv]][, left(ii)] * 
-                                       P$lcstat[[i]][[lv]][, right(ii)])
+          lccov[[i]][[lv]][, ii] <- (lcpstat[[i]][[lv]][, ii] - 
+                                       lcstat[[i]][[lv]][, left(ii)] * 
+                                       lcstat[[i]][[lv]][, right(ii)])
           M[right(ii), left(ii)] <- M[right(ii), left(ii)] + 
                                      lccov[[i]][[lv]][j, ii] * 
-                                     P$ldstat[[i]][j, lv]
+                                     ldstat[[i]][j, lv]
           M[left(ii), right(ii)] <- M[right(ii), left(ii)]
         }  #ii
       }  #lv
-      P$LMV[[i]][[j]] <- M
+      LMV[[i]][[j]] <- M
     }  #j 
   }  #i
   
-  P <- P[order(names(P))] ## order elements by name alphabetically
+  P <- list(cpstat = cpstat,
+            cstat = cstat, 
+            cstat2 = cstat2,
+            cvar = cvar,
+            dstat = dstat,
+            lcpstat = lcpstat, 
+            lcstat = lcstat, 
+            lcstat2 = lcstat,
+            ldstat = ldstat,
+            LMV = LMV,
+            MVMV = MVMV,
+            ostat = ostat,
+            ovar = ovar,
+            pistat = pstat,
+            W = W)
+  
   class(P) = "multimixParamList"
   return(P)
 }
