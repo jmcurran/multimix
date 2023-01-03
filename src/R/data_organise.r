c81d0a5c385dfa319908e81636a1fe46e4f7bf63
jmcurran <james.m.curran@gmail.com>
2023-01-03 18:37:37 +1300

    Fixed somethings. Not sure what

--- src/R/data_organise.r
+++ src/R/data_organise.r
@@ -52,7 +52,13 @@
 #' @examples
 #' data(cancer.df)
 #' mmObj = data_organise(cancer.df, numClusters = 2)
-data_organise <- function(dframe, numClusters, numIter = 1000, cdep = NULL, lcdep = NULL, minpstar = 1e-09) {
+data_organise <- function(dframe, 
+                          numClusters, 
+                          numIter = 1000, 
+                          cdep = NULL, 
+                          lcdep = NULL, 
+                          minpstar = 1e-09) {
+  
   discvar <- sapply(dframe, is.factor)  # logical indicator for discrete columns
   #
   n <- nrow(dframe)
@@ -60,10 +66,12 @@ data_organise <- function(dframe, numClusters, numIter = 1000, cdep = NULL, lcde
 
   ld <- rep(FALSE, v)
   lc <- rep(FALSE, v)
+  
   for (i in seq_along(lcdep)) {
     ld[lcdep[[i]][1]] <- TRUE  # logical indicator for discrete columns in location model
     lc[lcdep[[i]][-1]] <- TRUE  # logical indicator for continuous columns in location model
   }
+  
   md <- discvar & !ld
   mc <- !discvar & !lc
   oc <- mc
@@ -75,62 +83,116 @@ data_organise <- function(dframe, numClusters, numIter = 1000, cdep = NULL, lcde
   lclink <- seq_len(v)[lc]  # vector of continuous columns inside location cells
   lcdisc <- rep(NA, length(lcdep))
   op <- length(olink)
-  for (i in seq_along(lcdep)) lcdisc[i] <- lcdep[[i]][1]  # discrete location columns in lcdep order
+  
+  for (i in seq_along(lcdep)){ 
+    lcdisc[i] <- lcdep[[i]][1]  # discrete location columns in lcdep order
+  }
+  
   # number of levels of discrete variables:
   dlevs <- apply(dframe[, dlink, drop = FALSE], 2, count.unique)  # needs all rows
   ldlevs <- apply(dframe[, lcdisc, drop = FALSE], 2, count.unique)
   dvals <- list()
   ldvals <- list()
-  for (i in seq_along(dlink)) dvals[[i]] <- model.matrix(~0 + dframe[, dlink[i]])
-  for (i in seq_along(lcdisc)) ldvals[[i]] <- model.matrix(~0 + dframe[, lcdisc[i]])
+  
+  for (i in seq_along(dlink)){ 
+    dvals[[i]] <- model.matrix(~0 + dframe[, dlink[i]])
+  }
+  
+  for (i in seq_along(lcdisc)){ 
+    ldvals[[i]] <- model.matrix(~0 + dframe[, lcdisc[i]])
+  }
+  
   ovals <- as.matrix(dframe[, olink])
   ovals2 <- ovals^2
   cvals <- cvals2 <- list()
+  
   for (i in seq_along(cdep)) {
     cvals[[i]] <- as.matrix(dframe[, cdep[[i]], drop = FALSE])
     cvals2[[i]] <- cvals[[i]]^2
   }
+  
   lcvals <- lcvals2 <- list()
+  
   for (i in seq_along(lcdep)) {
     lcvals[[i]] <- as.matrix(dframe[, lcdep[[i]][-1], drop = FALSE])
     lcvals2[[i]] <- lcvals[[i]]^2
   }
+  
   cprods <- list()
+  
   for (i in seq_along(cdep)) {
     lcdi <- length(cdep[[i]])
     nxv <- lcdi * (lcdi - 1)/2
     cprods[[i]] <- matrix(NA, nrow = n, ncol = nxv)
+    
     for (ii in seq_len(nxv)) {
-      cprods[[i]][, ii] <- as.matrix(dframe[, cdep[[i]][left(ii)]] * dframe[, cdep[[i]][right(ii)], drop = FALSE])
+      cprods[[i]][, ii] <- as.matrix(dframe[, cdep[[i]][left(ii)]] *
+                                    dframe[, cdep[[i]][right(ii)], drop = FALSE])
     }
   }
+  
   lcprods <- list()
+  
   for (i in seq_along(lcdep)) {
     lcdepi <- lcdep[[i]][-1]
     lcdi <- length(lcdepi)
-    nxv <- lcdi * (lcdi - 1)/2
+    nxv <- lcdi * (lcdi - 1) / 2
     lcprods[[i]] <- matrix(NA, nrow = n, ncol = nxv)
+  
     for (ii in seq(length = nxv)) {
-      lcprods[[i]][, ii] <- as.matrix(dframe[, lcdepi[left(ii)]] * dframe[, lcdepi[right(ii)], drop = FALSE])
+      lcprods[[i]][, ii] <- as.matrix(dframe[, lcdepi[left(ii)]] *
+                                      dframe[, lcdepi[right(ii)], drop = FALSE])
     }
   }
 
   ldxc <- list()
+  
   for (i in seq_along(lcdep)) {
     ldxc[[i]] <- list()
     nlcd <- max(0, length(lcdep[[i]]) - 1)
+    
     for (j in seq_len(ldlevs[i])) {
       ldxc[[i]][[j]] <- matrix(NA, nrow = n, ncol = nlcd)
+    
       for (k in seq_len(nlcd)) {
         ldxc[[i]][[j]][, k] <- ldvals[[i]][, j] * lcvals[[i]][, k]
       }
     }
   }
-  D <- list(cdep = cdep, clink = clink, cprods = cprods, cvals = cvals, cvals2 = cvals2, dframe = dframe, discvar = discvar,
-    dlevs = dlevs, dlink = dlink, dvals = dvals, lc = lc, lcdep = lcdep, lcdisc = lcdisc, lclink = lclink,
-    lcprods = lcprods, lcvals = lcvals, lcvals2 = lcvals2, ld = ld, ldlevs = ldlevs, ldlink = ldlink, ldvals = ldvals,
-    ldxc = ldxc, mc = mc, md = md, minpstar = minpstar, n = n, numIter = numIter, oc = oc, olink = olink, op = op,
-    ovals = ovals, ovals2 = ovals2, numClusters = numClusters)
+  
+  D <- list(cdep = cdep, 
+            clink = clink, 
+            cprods = cprods, 
+            cvals = cvals, 
+            cvals2 = cvals2, 
+            dframe = dframe, 
+            discvar = discvar,
+            dlevs = dlevs, 
+            dlink = dlink, 
+            dvals = dvals, 
+            lc = lc, 
+            lcdep = lcdep, 
+            lcdisc = lcdisc, 
+            lclink = lclink,
+            lcprods = lcprods, 
+            lcvals = lcvals, 
+            lcvals2 = lcvals2, 
+            ld = ld, 
+            ldlevs = ldlevs, 
+            ldlink = ldlink, 
+            ldvals = ldvals,
+            ldxc = ldxc, 
+            mc = mc, 
+            md = md, 
+            minpstar = minpstar, 
+            n = n, 
+            numIter = numIter, 
+            oc = oc, 
+            olink = olink, 
+            op = op,
+            ovals = ovals, 
+            ovals2 = ovals2, 
+            numClusters = numClusters)
 
   class(D) = "multimixSettings"
   return(D)
