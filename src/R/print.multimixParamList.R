18a577fdbab87c3ddd25bce7f162c24b1b0f0367
jmcurran <j.curran@auckland.ac.nz>
2023-01-09 18:13:56 +1300

    starting on print methods

---
+++ src/R/print.multimixParamList.R
@@ -0,0 +1,10 @@
+#' S3 printing method for for multimix parameter results 
+#'
+#' @param x an object of class \code{multimixParamResults} -- see 
+#' \code{\link{initParamList}} for more information.
+#' @param ... additional arguments passed to \code{print}.
+#'
+#' @export
+print.multimixParamList = function(x, ...){
+  
+}
