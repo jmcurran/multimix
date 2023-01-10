46b28f0e77a402dd4a320b0869a6e99ef92f7a73
James Curran <james.m.curran@gmail.com>
2023-01-10 16:01:00 +1300

    More work on names and printing

--- src/R/print.multimixParamList.R
+++ src/R/print.multimixParamList.R
@@ -6,31 +6,63 @@
 #' means will be displayed for each univariate continuous variable, the cluster proportions
 #' for each level of a categorical variable, and the mean vector for each cluster and each
 #' multivariate normal variable.
+#' @param digits a vector of length 3. The first value determines how many decimal places to 
+#' round categorical proportions to. The second value determines how many significant digits to 
+#' display means to, and the third how many siginificant digits to display variances to. By default
+#' proportions are rounded to 4 decimal places, means 2 significant digits, and variances 3 significant 
+#' digits
 #' @param ... additional arguments passed to \code{print}.
 #'
 #' @export
-print.multimixParamList = function(x, type = c("means", "vars"), ...){
-  if(length(x$dstat) > 0){
+print.multimixParamList = function(x, type = c("means", "vars"), digits = c(4, 2, 3), ...){
+  
+  numCatVars = length(x$dstat)
+  numUVNVars = ncol(x$ostat)
+  numMVNVars = length(x$cstat)
+  
+  if(numCatVars > 0){
     cat(paste0("Categorical variables:\n"))
-    cat(paste0("  ", paste0(names(x$dstat), collapse = ", "), "\n"))
+    cat(paste0("  ", paste0(names(x$dstat), collapse = ", "), "\n\n"))
   }
   
-  if(ncol(x$ostat) > 0){
-    cat(paste0("\nUnivariate normal variables:\n"))
-    cat(paste0("  ", paste0(colnames(x$ostat), collapse = ", "), "\n"))
+  if(numUVNVars > 0){
+    cat(paste0("Univariate normal variables:\n"))
+    cat(paste0("  ", paste0(colnames(x$ostat), collapse = ", "), "\n\n"))
   }
   
-  if(length(x$cstat) > 0){
-    cat(paste0("\nMultivariate normal variables:\n"))
+  if(numMVNVars > 0){
+    cat(paste0("Multivariate normal variables:\n"))
     cells = sapply(x$cstat, function(cell){
                      paste0('(', paste0(colnames(cell), collapse = ", "), ')\n')
                   })
     cat(paste0("  Cell ", 1:length(cells), ": ", cells))
+    cat("\n")
   }
   
   type = match.arg(type)
   
   if(type == "means"){
+    if(numCatVars > 0){
+      cat("Category proportions by cluster and level for categorical variables\n")
+      cat("===================================================================\n")
+      for(v in names(x$dstat)){
+        cat(paste0("\nVariable ", v, ":\n"))
+        print(round(x$dstat[[v]], digits = digits[1]))
+        cat("\n")
+      }
+      cat("\n")
+    }
+    
+    if(numUVNVars > 0){
+      cat("Cluster means for univariate continuous variables\n")
+      cat("===================================================================\n")
+      for(v in colnames(x$ostat)){
+        cat(paste0("\nVariable ", v, ":\n"))
+        print(signif(x$ostat[,v, drop = FALSE], digits = digits[2]))
+        cat("\n")
+      }
+      cat("\n")
+    }
     
   }else{ ## vars
     
--- src/R/setNames.R
+++ src/R/setNames.R
@@ -2,7 +2,7 @@ setNames = function(mmResObj){
   numGroups = ncol(mmResObj$Z)
   colnames(mmResObj$Z) = paste0("Pr(x[i,] in G", 1:numGroups, ")")
   
-  if(length(D$dlink) > 0){
+  if(length(mmResObj$D$dlink) > 0){
     names(mmResObj$P$dstat) = names(D$dframe[, D$dlink])
     mmResObj$P$dstat = lapply(mmResObj$P$dstat, function(mat){
       colnames(mat) = gsub("^[^]]+[]]{2}(.*$)", "\\1", colnames(mat))
@@ -11,7 +11,9 @@ setNames = function(mmResObj){
     })
   }
   
-  
-  
+  if(ncol(mmResObj$P$ostat) > 0){
+    rownames(mmResObj$P$ostat) = paste0("G", 1:numGroups, ":")
+  }
+
   return(mmResObj)
 }
