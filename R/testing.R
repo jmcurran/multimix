bacd1417c37c772279c5cb3774904ac805170110
jmcurran <j.curran@auckland.ac.nz>
2023-02-01 10:09:23 +1300

    At least got Rcpp added to package

--- R/testing.R
+++
@@ -1,7 +0,0 @@
-#' This file contains functions we are testing. As a consequence, none of them 
-#' are exported, and will need the ::: operator to use them
-myfacto <- function(num) {
-  ##dyn.load("facto.so")
-  retvals <- .Fortran("facto",n = as.integer(num), answer = as.integer(1))
-  return(retvals$answer)
-}
